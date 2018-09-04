be4f65d30cbf70657decdf793c3abbf76035ec39
Bruce Hillsworth <bruce.hillsworth@gmail.com>
2018-09-04 21:55:56 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -99,7 +99,7 @@ class TicTacToe
       turn
     end
     if won?
-      puts "Congratulations #{winner(board)}!"
+      puts "Congratulations #{winner}!"
     else
       puts "Cat's Game!"
     end
