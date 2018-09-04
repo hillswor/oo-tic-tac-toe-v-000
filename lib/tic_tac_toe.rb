1b22c303b8e3caa282135528407efc9d36cb8e8d
Bruce Hillsworth <bruce.hillsworth@gmail.com>
2018-09-04 21:48:20 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -67,9 +67,9 @@ class TicTacToe
     WIN_COMBINATIONS.detect do|combo|
       if (@board[combo[0]] == "X" && @board[combo[1]] == "X" && @board[combo[2]] == "X" || @board[combo[0]] == "O" && @board[combo[1]] == "O" && @board[combo[2]] == "O")
         return combo
+      end
     end
   end
-end
 
   def full?
     @board.all?{|moves| moves == "X" || moves == "O"}
